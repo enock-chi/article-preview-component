c5fa97055b428ef818c50e9564c656e264536050
Enock Chikobvu <sechikobvu@gmail.com>
2021-10-23 19:47:09 +0200

    2nd draft
    share button wont work

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+document.querySelector(".button").onclick = function() {
+  let name = document.getElementById("social-media");
+  name.setAttribute("visibility", "true");
+  console.log(name);
+
+}
